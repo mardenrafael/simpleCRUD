8db0a4ab939b01e81820337a6c206c13a75d695f
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-08-16 22:10:16 -0400

    Update dataSource

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -4,12 +4,23 @@ import { DataSource } from "typeorm";
 
 dotenv.config();
 
-export const AppDataSourceOptions = new DataSource({
+const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.TYPEORM_HOST,
   port: Number(process.env.TYPEORM_PORT),
   username: process.env.TYPEORM_USER,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
-  migrations: ["src/database/migrations/*.ts"],
+  migrations: [process.env.TYPEORM_MIGRATIONS],
+  entities: [process.env.TYPEORM_ENTITIES],
 });
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source initialize");
+  })
+  .catch(err => {
+    console.log("Error on initialize Data Source: ", err);
+  });
+
+export { AppDataSource };
