2911e6820097d30cbdfc075b087b9e7f3c01fcc8
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-08-16 22:10:39 -0400

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import "reflect-metadata";
 import express from "express";
+import "./database/dataSource";
+import { routes } from "./routes";
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3000, () => {
   console.log("Server runnig");
