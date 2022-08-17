63cf759673c901597898e6b456011524f3f30db8
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-08-16 22:12:11 -0400

    Create category Service

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,28 @@
+import { AppDataSource } from "../database/dataSource";
+import { Category } from "../entities/Category";
+
+type CategoryRequest = {
+  name: string;
+  description: string;
+};
+
+export class CreateCategoryService {
+  async execute({
+    name,
+    description,
+  }: CategoryRequest): Promise<Category | Error> {
+    const repo = AppDataSource.getRepository(Category);
+
+    if (await repo.findOneBy({ name: name })) {
+      return new Error("Category already exist");
+    }
+    const category = repo.create({
+      name,
+      description,
+    });
+
+    await repo.save(category);
+
+    return category;
+  }
+}
