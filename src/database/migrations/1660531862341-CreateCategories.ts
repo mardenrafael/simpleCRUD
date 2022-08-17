0453a75f2eca40b75df6371511956238aa741be0
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-08-16 22:11:11 -0400

    update CreateCategorie migration

--- src/database/migrations/1660531862341-CreateCategories.ts
+++ src/database/migrations/1660531862341-CreateCategories.ts
@@ -33,7 +33,7 @@ export class CreateCategories1660531862341
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    // await queryRunner.dropTable("categories");
+    await queryRunner.dropTable("categories");
   }
 }
 
